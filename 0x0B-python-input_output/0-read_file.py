85098bca72df03054df7333682d4647ecc1a1c36
Emmanuel Mbumila <mbuxcompany@gmail.com>
2023-04-12 06:31:02 -0700

    fix:file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,6 @@
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as myFile:
-        print(myFile.read(), end="")
+    """ reading the text file (UTF8) and print the stadout """
+    with open(filename, "r", encoding="UTF-8") as f:
+        print(f.read(), end="")
