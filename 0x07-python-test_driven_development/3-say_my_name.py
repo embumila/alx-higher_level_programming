b321cf55d040d639f3f3d1045ecd80cece4ed27d
Emmanuel Mbumila <mbuxcompany@gmail.com>
2023-04-18 10:52:56 -0700

    fix:file

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,8 +3,8 @@
 """Module for say_my_name method"""
 
 def say_my_name(first_name, last_name=""):
-    if type(first_name) != string:
-        raise TypeErro("first_name must be a string")
-    else if type(last_name) != string:
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
         raise TypeError ("last_name must be  a string")
-    print ("My name is{:s}  {:s}".format(first_name + last_name))
+    print ("My name is {:s}  {:s}".format(first_name, last_name))
