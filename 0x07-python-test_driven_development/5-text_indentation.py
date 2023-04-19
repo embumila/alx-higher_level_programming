4edae9a6ce92d14dba467019a97e3d88a436ad4d
Emmanuel Mbumila <mbuxcompany@gmail.com>
2023-04-19 08:18:16 -0700

    add: file

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""text indent"""
 
-def text_identation(txt):
-    my_string = str(txt)
+
+def text_indentation(text):
     start_index = 0
-    if type(my_string) != str:
-        raise TypeError("text must be string")
-    for i,element in enumerate(my_string):
-        if not str(element).isalnum() and element in ['?','.',':']:
-            print(my_string[start_index:i+1],end='\n\n')
+    if type(text) != str:
+        raise TypeError('text must be string')
+    for i, element in enumerate(text):
+        if element in ["?", ".", ":", "\n\n"]:
+            print(text[start_index : i + 1], end="\n\n")
             start_index = i + 1
+
+    # Print the remaining text after the last special character or newline
+    if start_index < len(text):
+        print(text[start_index:])
