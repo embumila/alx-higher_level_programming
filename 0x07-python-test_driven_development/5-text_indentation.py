adea5884962aa21ce7b9f145850aa6908af15518
Emmanuel Mbumila <mbuxcompany@gmail.com>
2023-04-19 06:19:42 -0700

    add: file

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""text indent"""
 
-
-def text_indentation(text):
-    """text indent"""
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
-
-    for delimeter in "?:.":
-        words = (delimeter + "\n\n").join(
-                [index.strip(" ") for index in words.split(delimeter)])
+def text_identation(txt):
+    my_string = str(txt)
+    start_index = 0
+    if type(my_string) != str:
+        raise TypeError("text must be string")
+    for i,element in enumerate(my_string):
+        if not str(element).isalnum() and element in ['?','.',':']:
+            print(my_string[start_index:i+1],end='\n\n')
+            start_index = i + 1
